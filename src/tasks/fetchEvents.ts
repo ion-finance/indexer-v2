caa5d8eee3456a6ec9fbf83e5b2867878cbaf8f0
alice-ion <alice@ionfi.xyz>
2023-07-26 22:19:59 +0900

    Fetch events gracefully

--- src/tasks/fetchEvents.ts
+++ src/tasks/fetchEvents.ts
@@ -6,7 +6,9 @@ const fetchEvents = async () => {
   const timestamp = await prisma.indexerState.getLastTimestamp();
   const routerAddress = "EQDPba4MHzwT7Q8YT_qJhzRs4sfUDqCCxkS_Fn642yjPH-71";
   const res = await axios(
-    `${process.env.TON_API_URL}/accounts/${routerAddress}/events?start_date=${timestamp}&limit=100`
+    `${process.env.TON_API_URL}/accounts/${routerAddress}/events?start_date=${
+      timestamp - 10
+    }&limit=100`
   );
 
   const events = res.data.events as Event[];
