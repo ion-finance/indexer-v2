78d740f75be171ccbc95fb3525a8c6e7360e966b
alice-ion <alice@ionfi.xyz>
2023-07-26 17:28:06 +0900

    Reuse db connections

--- src/clients/prisma/index.ts
+++ src/clients/prisma/index.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import PoolExtensions from "./extensions/pool";
 import IndexerStateExtenstions from "./extensions/indexer";
 
-const prisma = new PrismaClient()
-  .$extends(PoolExtensions)
-  .$extends(IndexerStateExtenstions);
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient().$extends(PoolExtensions).$extends(IndexerStateExtenstions);
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
