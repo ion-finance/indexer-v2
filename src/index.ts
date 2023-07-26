66aa9b7b81be1d083295bbd26b0ab36277d1761f
alice-ion <alice@ionfi.xyz>
2023-07-26 21:21:43 +0900

    Reduce pooling time

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { refreshDailyVolume } from "./tasks/refreshVoulme";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const MIN_POOL = 4000; // 4s
+const MIN_POOL = 2000; // 2s
 
 const eventPooling = async () => {
   const events = await fetchEvents();
