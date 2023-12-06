f9eef2e7a3f116480e06798c8e37a16193bdd12c
alice-ion <alice@ionfi.xyz>
2023-12-06 15:16:00 +0900

    Update migration

--- prisma/migrations/20231206061530_init/migration.sql
+++ prisma/migrations/20231206061530_init/migration.sql
@@ -1,4 +1,32 @@
 -- CreateTable
+CREATE TABLE "Pool" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "tokenXAddress" TEXT NOT NULL,
+    "tokenYAddress" TEXT NOT NULL,
+    "binStep" INTEGER NOT NULL,
+    "activeBinId" INTEGER NOT NULL,
+
+    CONSTRAINT "Pool_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "jettonMinterAddress" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "decimals" INTEGER NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "DepositedToBins" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
