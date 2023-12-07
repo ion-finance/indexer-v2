d829efa6c283c303c013d5e158d14b60169d48d5
alice-ion <alice@ionfi.xyz>
2023-12-07 17:01:06 +0900

    Impl swap mapping

--- src/mappings/pool.ts
+++ src/mappings/pool.ts
@@ -3,6 +3,7 @@ import prisma from "../clients/prisma";
 import { Prisma } from "@prisma/client";
 import parseInitialized from "../parsers/parseInitialized";
 import fetchTokenData from "../utils/fetchTokenData";
+import parseSwap from "../parsers/parseSwap";
 
 export const handleInitialized = async (
   event: Event<ReturnType<typeof parseInitialized>>
@@ -153,4 +154,55 @@ export const handleDepositedToBins = async (
       }
     })
   );
+};
+
+export const handleSwap = async (
+  event: Event<ReturnType<typeof parseSwap>>
+) => {
+  console.log("Swap event is indexed.");
+  console.log(event);
+
+  await prisma.swap.upsert({
+    where: {
+      id: event.transaction.hash,
+    },
+    update: {
+      timestamp: event.transaction.timestamp,
+      poolAddress: event.transaction.source,
+      senderAddress: event.params.senderAddress,
+      receiverAddress: event.params.receiverAddress,
+      amountIn: event.params.amountIn.toString(),
+      amountOut: event.params.amountOut.toString(),
+      swapForY: event.params.swapForY,
+      activeBinId: event.params.activeBinId,
+    },
+    create: {
+      id: event.transaction.hash,
+      timestamp: event.transaction.timestamp,
+      poolAddress: event.transaction.source,
+      senderAddress: event.params.senderAddress,
+      receiverAddress: event.params.receiverAddress,
+      amountIn: event.params.amountIn.toString(),
+      amountOut: event.params.amountOut.toString(),
+      swapForY: event.params.swapForY,
+      activeBinId: event.params.activeBinId,
+    },
+  });
+
+  const pool = await prisma.pool.findFirst({
+    where: {
+      id: event.transaction.source,
+    },
+  });
+
+  if (pool) {
+    await prisma.pool.update({
+      where: {
+        id: event.transaction.source,
+      },
+      data: {
+        activeBinId: event.params.activeBinId,
+      },
+    });
+  }
 };
---
+++ src/parsers/parseSwap.ts
@@ -0,0 +1,24 @@
+import { Cell } from "@ton/core";
+
+const parseSwap = (message: Cell) => {
+  const body = message.beginParse();
+  const logCode = body.loadUint(32);
+  const senderAddress = body.loadAddress().toString();
+  const receiverAddress = body.loadAddress().toString();
+  const activeBinId = body.loadUint(24);
+  const amountIn = body.loadCoins();
+  const amountOut = body.loadCoins();
+  const swapForY = body.loadUint(1) ? true : false;
+
+  return {
+    logCode,
+    senderAddress,
+    receiverAddress,
+    activeBinId,
+    amountIn,
+    amountOut,
+    swapForY,
+  };
+};
+
+export default parseSwap;
