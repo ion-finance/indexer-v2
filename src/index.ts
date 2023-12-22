d85fe2088bb5abdedda0a0c7c77d49accc49d1f0
alice-ion <alice@ionfi.xyz>
2023-12-22 13:32:31 +0900

    Impl temp token & pool api

--- src/index.ts
+++ src/index.ts
@@ -220,6 +220,54 @@ app.get("/accounts/:address/positions", async function handler(req, res) {
   });
 });
 
+app.get("/tokens", async function handler(req, res) {
+  const tokens = await prisma.token.findMany();
+
+  return res.json(
+    tokens.map((token) => {
+      return {
+        ...token,
+        priceUsd: 0.999967, // mock
+        apy: 7.23, // mock
+      };
+    })
+  );
+});
+
+app.get("/pools", async function handler(req, res) {
+  const pools = await prisma.pool.findMany();
+  const tokens = await prisma.token.findMany();
+
+  return res.json(
+    pools.map((pool) => {
+      const tokenX = tokens.find((token) => token.id === pool.tokenXAddress);
+      const tokenY = tokens.find((token) => token.id === pool.tokenYAddress);
+
+      return {
+        ...pool,
+        tokenX: {
+          ...tokenX,
+          priceUsd: 0.999967, // mock
+          apy: 7.23, // mock
+        },
+        tokenY: {
+          ...tokenY,
+          priceUsd: 0.999967, // mock
+          apy: 7.23, // mock
+        },
+        // mock data
+        reserveX: "4254160587174121",
+        reserveY: "8354160587174",
+        totalSupply: "2550988259892",
+        liquidityUsd: 17732929.594,
+        volumeUsd: 18123142.156,
+        feesUsd: 12123,
+        apy: 12.15,
+      };
+    })
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
