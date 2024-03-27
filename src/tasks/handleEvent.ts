f82049d69df9c3a5bc7e684a74b75d67bb8398e1
devlbrian <brian@ionfi.xyz>
2024-03-27 20:34:50 +0900

    Impl handleEvent

--- src/tasks/handleEvent.ts
+++ src/tasks/handleEvent.ts
@@ -1,184 +1,238 @@
-import axios, { AxiosError } from "axios";
-import { TransactionResult } from "../types/ton-api";
-import { Address, Cell } from "@ton/core";
-import {
-  handleDepositedToBins,
-  handleInitialized,
-  handleSwap,
-  handleTransferBatch,
-  handleWithdrawnFromBins,
-  handleOrderPlaced,
-  handleOrderCancelled,
-  handleOrderClaimed,
-  handleOrderExecuted,
-} from "../mappings/clmm";
+import axios from "axios";
+import { AccountEvent, TransactionResult } from "../types/ton-api";
+
 import {
   handleAddLiquidity,
   handleExchange,
   handlePoolCreated,
   handleRemoveLiquidity,
 } from "../mappings/cpmm";
+import { parseRaw } from "../utils/address";
+import dotenv from "dotenv";
+dotenv.config();
+const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS;
 
-// CLMM
-const DEPOSITED_TO_BINS = "0xafeb11ef";
-const INITIALIZED = "0x9bb3a52e";
-const SWAP = "0x25938561";
-const TRANSFER_BATCH = "0x5b8fa78c";
-const WITHDRAWN_FROM_BINS = "0xf6172b31";
-const ORDER_PLACED = "0xbef3f947";
-const ORDER_CANCELLED = "0x4572803f";
-const ORDER_EXECUTED = "0x18b134be";
-const ORDER_CLAIMED = "0x5e586bc8";
+enum OP {
+  TRANSFER = "0x0f8a7ea5",
+  TRANSFER_NOTIFICATION = "0x7362d09c",
+  PROVIDE_LP = "0xfcf9e58f",
+  PAY_TO = "0xf93bb43f",
+  ADD_LIQUIDITY = "0x3ebe5431",
+  CB_ADD_LIQUIDITY = "0x56dfeb8a",
+  INTERNAL_TRANSFER = "0x178d4519",
+  EXCESS = "0xd53276db",
+  SWAP = "0x25938561",
+}
 
-// CPMM
-const ADD_LIQUIDITY = "0x01d3037e";
-const REMOVE_LIQUIDITY = "0xa95d7721";
-const EXCHANGE = "0xbd687ba6";
-const POOL_CREATED = "0x7d0e1322";
+enum CUSTOM_OP {
+  // custom ops
+  EXT_IN_MSG = "ext_in_msg",
+  ROUTER_DEPLOYED = "router_deployed",
+  POOL_DEPLOYED = "pool_deployed",
+  ROUTER_JETTON_WALLET_DEPLOYED = "router_jetton_wallet_deployed",
+  LP_WALLET_DEPLOYED = "lp_wallet_deployed",
+  LP_ACCOUNT_DEPLOYED = "lp_account_deployed",
+}
+function createBiDirectionalEnum<E extends { [index: string]: string }>(
+  e: E
+): E & { [key: string]: string } {
+  const reverseEntries = Object.entries(e).map(([key, value]) => [value, key]);
+  return Object.fromEntries([...Object.entries(e), ...reverseEntries]) as E & {
+    [key: string]: string;
+  };
+}
+
+export const BiDirectionalOP = createBiDirectionalEnum(OP);
+export const BiDirectionalCustomOP = createBiDirectionalEnum(CUSTOM_OP);
 
 // Info
 // * This method can throw an error if the event is processing
-const handleEvent = async (event_id: string) => {
+const handleEvent = async (event: AccountEvent) => {
+  const { event_id, actions } = event;
   const res = await axios(`${process.env.TON_API_URL}/traces/${event_id}`, {
     headers: {
       Authorization: `Bearer ${process.env.TON_API_KEY}`,
     },
   });
 
-  const transactionRes = res.data as TransactionResult;
+  const firstActionType = actions[0].type;
+  const isSwap = firstActionType === "JettonSwap";
 
-  let txs = [transactionRes];
-  const txsHasOutMsgs: TransactionResult[] = [];
-  while (txs.length != 0) {
-    let new_txs: TransactionResult[] = [];
+  const traces = res.data as TransactionResult;
 
-    txs.forEach((t) => {
-      if (t.transaction.out_msgs.length > 0) {
-        txsHasOutMsgs.push({
-          ...t,
-          children: undefined,
-        });
-      }
+  // Extract paths
+  // TODO: implement test code
+  const paths: Ops[][] = extractPaths(traces);
+  // console.log("paths", paths);
+  const isRouterDeployed = paths.some((path) =>
+    path.some(
+      ({ customOp }) => customOp === BiDirectionalCustomOP.ROUTER_DEPLOYED
+    )
+  );
 
-      if (t.children) {
-        new_txs = [...new_txs, ...t.children];
-      }
-    });
+  const isPoolDeployed = paths.some((path) =>
+    path.some(
+      ({ customOp }) => customOp === BiDirectionalCustomOP.POOL_DEPLOYED
+    )
+  );
 
-    txs = new_txs;
+  const isLpWalletDeployed = paths.some((path) =>
+    path.some(
+      ({ customOp }) => customOp === BiDirectionalCustomOP.LP_WALLET_DEPLOYED
+    )
+  );
+
+  const isLpAccountDeployed = paths.some((path) =>
+    path.some(
+      ({ customOp }) => customOp === BiDirectionalCustomOP.LP_ACCOUNT_DEPLOYED
+    )
+  );
+
+  const isRouterJettonWalletDeployed = paths.some((path) =>
+    path.some(
+      ({ customOp }) =>
+        customOp === BiDirectionalCustomOP.ROUTER_JETTON_WALLET_DEPLOYED
+    )
+  );
+
+  const isProvideLpConfirmed = paths.some((path) =>
+    path.some(({ op }) => op === BiDirectionalOP.CB_ADD_LIQUIDITY)
+  );
+
+  const isProvideLp =
+    !isProvideLpConfirmed &&
+    paths.some((path) =>
+      path.some(({ op }) => op === BiDirectionalOP.ADD_LIQUIDITY)
+    );
+
+  // deploy cases can be overlapped
+  if (isRouterDeployed) {
+    console.log(`Router deploy event: ${event_id}`);
+  }
+  if (isPoolDeployed) {
+    console.log(`Pool deploy event: ${event_id}`);
+    handlePoolCreated({ event, traces });
+  }
+  if (isLpWalletDeployed) {
+    console.log(`LpWallet deploy event: ${event_id}`);
+  }
+  if (isLpAccountDeployed) {
+    console.log(`LpAccount deploy event: ${event_id}`);
+  }
+  if (isRouterJettonWalletDeployed) {
+    console.log(`Router Jetton Wallet deploy event: ${event_id}`);
   }
 
-  for (let i = 0; i < txsHasOutMsgs.length; i++) {
-    const tx = txsHasOutMsgs[i];
-    for (let j = 0; j < tx.transaction.out_msgs.length; j++) {
-      const msg = tx.transaction.out_msgs[j];
-      const body = Cell.fromBoc(Buffer.from(msg.raw_body, "hex"))[0];
-      const transaction = {
-        source: Address.parseRaw(tx.transaction.account.address).toString(),
-        hash: tx.transaction.hash,
-        timestamp: tx.transaction.utime,
-        eventId: event_id,
-      };
+  if (isSwap) {
+    console.log(`Exchange event: ${event_id}`);
+    handleExchange({ event, traces });
+  } else if (isProvideLp) {
+    console.log(`Provide Lp event: ${event_id}`);
+  } else if (isProvideLpConfirmed) {
+    console.log(`Provide Lp Confirmed: ${event_id}`);
+    handleAddLiquidity({ event, traces });
+  }
 
-      switch (msg.op_code) {
-        case DEPOSITED_TO_BINS: {
-          await handleDepositedToBins({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case INITIALIZED: {
-          await handleInitialized({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case SWAP: {
-          await handleSwap({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case TRANSFER_BATCH: {
-          await handleTransferBatch({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case WITHDRAWN_FROM_BINS: {
-          // !NOTE: disable withdraw on version pre-alpha-contract
-          // await handleWithdrawnFromBins({
-          //   transaction,
-          //   body,
-          // });
-          break;
-        }
-        case ORDER_PLACED: {
-          await handleOrderPlaced({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case ORDER_CANCELLED: {
-          await handleOrderCancelled({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case ORDER_CLAIMED: {
-          await handleOrderClaimed({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case ORDER_EXECUTED: {
-          await handleOrderExecuted({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case ADD_LIQUIDITY: {
-          await handleAddLiquidity({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case REMOVE_LIQUIDITY: {
-          await handleRemoveLiquidity({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case EXCHANGE: {
-          await handleExchange({
-            transaction,
-            body,
-          });
-          break;
-        }
-        case POOL_CREATED: {
-          await handlePoolCreated({
-            transaction,
-            body,
-          });
-          break;
-        }
-        default:
-          console.log("UNKNOWN OP_CODE:", msg.op_code);
-          break;
-      }
-    }
+  if (
+    !isRouterDeployed &&
+    !isPoolDeployed &&
+    !isLpWalletDeployed &&
+    !isLpAccountDeployed &&
+    !isRouterJettonWalletDeployed &&
+    !isSwap &&
+    !isProvideLp &&
+    !isProvideLpConfirmed
+  ) {
+    console.log(`Unknown event: ${event_id}`);
   }
 };
 
 export default handleEvent;
+
+// function extractPaths(node: TransactionResult): string[][] {
+function extractPaths(node: TransactionResult): Ops[][] {
+  // This function recursively extracts the paths using the spread operator.
+  function recurse(
+    currentNode: TransactionResult,
+    parentNode: TransactionResult | null,
+    path: Ops[],
+    isFirst?: boolean
+  ): void {
+    // If there's no op_code in the in_msg, use 'ext_in_msg' as op_code for the root transaction.
+    const { transaction } = currentNode;
+    const opCode = transaction.in_msg.op_code || "";
+    const customOp = (function () {
+      if (isFirst) {
+        return "ext_in_msg";
+      }
+      const isParentJettonMaster =
+        parentNode?.interfaces?.includes("jetton_master");
+      const { orig_status, end_status } = transaction;
+      if (orig_status === "nonexist" && end_status === "active") {
+        const sourceAddress = parseRaw(transaction.in_msg.source.address);
+        const destinationAddress = parseRaw(
+          transaction.in_msg.destination.address
+        );
+        // TODO: consider case of router jetton wallet deploy
+        if (destinationAddress === ROUTER_ADDRESS) {
+          return "router_deployed";
+        } else if (sourceAddress === ROUTER_ADDRESS) {
+          return "pool_deployed";
+        } else if (isParentJettonMaster) {
+          // parent should be jettonMaster && pool
+          if (opCode === BiDirectionalOP.ADD_LIQUIDITY) {
+            return "lp_account_deployed";
+          } else {
+            return "lp_wallet_deployed";
+          }
+        } else if (opCode === BiDirectionalOP.INTERNAL_TRANSFER) {
+          return "router_jetton_wallet_deployed";
+        } else {
+          return "contract_deployed";
+        }
+      }
+      return "";
+    })();
+
+    const opKey = BiDirectionalOP[opCode];
+    if (!opKey && !customOp) {
+      console.warn(
+        `Unknown opCode: ${opCode}, txHash: ${currentNode.transaction.hash}`
+      );
+    }
+    // If currentNode does not have children, it is a leaf.
+    if (!currentNode.children || currentNode.children.length === 0) {
+      paths.push([
+        ...path,
+        {
+          op: opCode,
+          opReadable: opKey,
+          customOp,
+        },
+      ]); // Use spread operator here
+      return;
+    }
+    // If currentNode has children, iterate through them and recurse.
+    currentNode.children.forEach((child) => {
+      recurse(child, currentNode, [
+        ...path,
+        {
+          op: opCode,
+          opReadable: opKey,
+          customOp,
+        },
+      ]); // Use spread operator here
+    });
+  }
+
+  const paths: Ops[][] = [];
+  // Start recursion with the root node.
+  recurse(node, null, [], true);
+  return paths;
+}
+
+type Ops = {
+  op: string;
+  opReadable?: string;
+  customOp: string;
+};
