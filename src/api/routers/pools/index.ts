087970538e555b8a2773ceaa8117b6ed208ff622
devlbrian <brian@ionfi.xyz>
2024-03-28 18:38:32 +0900

    Skip empty pool

--- src/api/routers/pools/index.ts
+++ src/api/routers/pools/index.ts
@@ -1,74 +1,74 @@
 import { Router } from "express";
 import prisma from "../../../clients/prisma";
 import { getPriceUsd } from "../../../mocks/price";
+import { compact } from "lodash";
 
 const router = Router();
 
 router.get("/pools", async function handler(req, res) {
-  const pools = await prisma.pool.findMany();
+  const rawPools = await prisma.pool.findMany();
   const tokens = await prisma.token.findMany();
   const bins = await prisma.bins.findMany();
 
-  return res.json(
-    pools.map((pool) => {
-      const tokenX = tokens.find((token) => token.id === pool.tokenXAddress);
-      const tokenY = tokens.find((token) => token.id === pool.tokenYAddress);
-      if (!tokenX) {
-        console.warn("TokenX not found", pool.tokenXAddress);
-        return;
-      }
+  const pools = rawPools.map((pool) => {
+    const tokenX = tokens.find((token) => token.id === pool.tokenXAddress);
+    const tokenY = tokens.find((token) => token.id === pool.tokenYAddress);
+    if (!tokenX) {
+      console.warn("TokenX not found", pool.tokenXAddress);
+      return;
+    }
 
-      if (!tokenY) {
-        console.warn("TokenY not found", pool.tokenYAddress);
-        return;
-      }
+    if (!tokenY) {
+      console.warn("TokenY not found", pool.tokenYAddress);
+      return;
+    }
 
-      const priceXUsd = getPriceUsd(tokenX?.symbol);
-      const priceYUsd = getPriceUsd(tokenY?.symbol);
+    const priceXUsd = getPriceUsd(tokenX?.symbol);
+    const priceYUsd = getPriceUsd(tokenY?.symbol);
 
-      let reserveData = {};
+    let reserveData = {};
 
-      if (pool.type === "CLMM") {
-        const poolBins = bins.filter((bin) => bin.poolAddress === pool.id);
+    if (pool.type === "CLMM") {
+      const poolBins = bins.filter((bin) => bin.poolAddress === pool.id);
 
-        reserveData = {
-          reserveX: poolBins
-            .reduce((acc, bin) => acc + BigInt(bin.reserveX), BigInt(0))
-            .toString(),
-          reserveY: poolBins
-            .reduce((acc, bin) => acc + BigInt(bin.reserveY), BigInt(0))
-            .toString(),
-          totalSupply: "2550988259892", // TODO
-        };
-      } else {
-        reserveData = {
-          reserveX: pool.reserveX,
-          reserveY: pool.reserveY,
-          totalSupply: pool.lpSupply,
-        };
-      }
-
-      return {
-        ...pool,
-        tokenX: {
-          ...tokenX,
-          priceUsd: priceXUsd,
-          apy: 7.23, // mock
-        },
-        tokenY: {
-          ...tokenY,
-          priceUsd: priceYUsd,
-          apy: 7.23, // mock
-        },
-        ...reserveData,
-        // mock data
-        liquidityUsd: 17732929.594,
-        volumeUsd: 18123142.156,
-        feesUsd: 12123,
-        apy: 12.16,
+      reserveData = {
+        reserveX: poolBins
+          .reduce((acc, bin) => acc + BigInt(bin.reserveX), BigInt(0))
+          .toString(),
+        reserveY: poolBins
+          .reduce((acc, bin) => acc + BigInt(bin.reserveY), BigInt(0))
+          .toString(),
+        totalSupply: "2550988259892", // TODO
       };
-    })
-  );
+    } else {
+      reserveData = {
+        reserveX: pool.reserveX,
+        reserveY: pool.reserveY,
+        totalSupply: pool.lpSupply,
+      };
+    }
+
+    return {
+      ...pool,
+      tokenX: {
+        ...tokenX,
+        priceUsd: priceXUsd,
+        apy: 7.23, // mock
+      },
+      tokenY: {
+        ...tokenY,
+        priceUsd: priceYUsd,
+        apy: 7.23, // mock
+      },
+      ...reserveData,
+      // mock data
+      liquidityUsd: 17732929.594,
+      volumeUsd: 18123142.156,
+      feesUsd: 12123,
+      apy: 12.16,
+    };
+  });
+  return res.json(compact(pools));
 });
 
 export default router;
