e61af8ce6f3b6f01f4997b9c4032407e1a7d6b6f
devlbrian <brian@ionfi.xyz>
2024-04-09 21:00:14 +0900

    Make api as folder

--- src/api/routers/tokenPrices/tokenPrices.ts
+++ src/api/routers/tokenPrices/tokenPrices.ts
@@ -1,5 +1,5 @@
-import router from './pools'
-import prisma from '../../clients/prisma'
+import router from '../pools'
+import prisma from '../../../clients/prisma'
 import { TokenPrice } from '@prisma/client'
 
 router.get('/token-prices', async function handler(req, res) {
--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
-// TODO: fix
+// TODO: update when re-deployed
 export const TON_WALLET_ADDRESS =
   'EQDXWP8l4GLJVjOIdp6AwLqvklmxa0rw19UNn5K-3_HDpmvy'
