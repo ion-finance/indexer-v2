d0bcacfe3fd3326257bdbf76f229b385ff90fede
devlbrian <brian@ionfi.xyz>
2024-04-09 14:19:39 +0900

    Add withdraw to trace api

--- src/api/routers/trace.ts
+++ src/api/routers/trace.ts
@@ -10,7 +10,11 @@ router.get('/traces/:trace_hash', async function handler(req, res) {
   const swap = await prisma.swap.findFirst({
     where: { id: trace_hash },
   })
-  const hasTrace = deposit || swap
+
+  const withdraw = await prisma.withdraw.findFirst({
+    where: { id: trace_hash },
+  })
+  const hasTrace = deposit || swap || withdraw
   return res.json(hasTrace)
 })
 
