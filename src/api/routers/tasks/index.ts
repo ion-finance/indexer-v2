6754621ba54de63d76b9bec69484c10429ee3400
devlbrian <brian@ionfi.xyz>
2024-04-25 13:18:42 +0900

    Implement api '/tasks/pool-creation'

--- src/api/routers/tasks/index.ts
+++ src/api/routers/tasks/index.ts
@@ -1,4 +1,7 @@
-import { Router } from 'express'
+import { TokenPrice } from '@prisma/client'
+import { NextFunction, Request, Response, Router } from 'express'
+import { query, validationResult } from 'express-validator'
+import { filter, isEmpty } from 'lodash'
 
 import prisma from 'src/clients/prisma'
 
@@ -26,5 +29,39 @@ router.get('/tasks/cpmm_beta', async function handler(req, res) {
     earnContractAt: deposit?.timestamp || deposit?.timestamp,
   })
 })
+
+router.get(
+  '/tasks/pool-creation',
+  query('creator')
+    .isString()
+    .notEmpty()
+    .withMessage('Owner address is required'),
+  async (req: Request, res: Response) => {
+    const { creator } = req.query as { creator: string }
+
+    if (!creator) {
+      return res.json({
+        status: 400,
+        data: [],
+      })
+    }
+    const pool = await prisma.pool.findFirst({
+      where: {
+        creator,
+      },
+    })
+    if (!pool) {
+      return res.json({ status: 404, data: [] })
+    }
+    return res.json({
+      status: 200,
+      data: {
+        poolId: pool.id,
+        creator: pool.creator,
+        createdAt: pool.createdAt,
+      },
+    })
+  },
+)
 
 export default router
