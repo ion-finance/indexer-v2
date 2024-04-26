90002e42ae5714642ed3a9162748b2a6f1f76c9a
devlbrian <brian@ionfi.xyz>
2024-04-26 18:39:20 +0900

    Remove unusings

--- src/api/routers/tasks/index.ts
+++ src/api/routers/tasks/index.ts
@@ -1,34 +1,9 @@
-import { TokenPrice } from '@prisma/client'
-import { NextFunction, Request, Response, Router } from 'express'
-import { query, validationResult } from 'express-validator'
-import { filter, isEmpty } from 'lodash'
+import { Request, Response, Router } from 'express'
+import { query } from 'express-validator'
 
 import prisma from 'src/clients/prisma'
 
 const router = Router()
-
-router.get('/tasks/cpmm_beta', async function handler(req, res) {
-  const { senderAddress } = req.query
-
-  if (!senderAddress) {
-    return res.json({
-      status: 400,
-      data: [],
-    })
-  }
-
-  const query = { senderAddress: senderAddress as string }
-
-  const [deposit, swap] = await Promise.all([
-    prisma.deposit.findFirst({ where: query }),
-    prisma.swap.findFirst({ where: query }),
-  ])
-
-  return res.json({
-    convertContractAt: swap?.timestamp || swap?.timestamp,
-    earnContractAt: deposit?.timestamp || deposit?.timestamp,
-  })
-})
 
 router.get(
   '/tasks/pool-creation',
