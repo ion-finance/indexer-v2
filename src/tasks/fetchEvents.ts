0cfbcd8acdf389007851f33ba728ae902af0d0b0
devlbrian <brian@ionfi.xyz>
2024-05-15 17:43:48 +0900

    TODO: remove, hardcode for escaping ton api error

--- src/tasks/fetchEvents.ts
+++ src/tasks/fetchEvents.ts
@@ -14,23 +14,31 @@ const fetchEvents = async ({
   timestamp?: number
 }) => {
   let beforeLt = 0
+  const baseUrl = `${process.env.TON_API_URL}/accounts/${routerAddress}/events`
   const events: AccountEvent[] = []
 
+  const fetch = async (start_date: number, before_lt: number) => {
+    // const args = `start_date=${start_date}&before_lt=${before_lt}&limit=100`
+    const args = (function () {
+      if (!before_lt) {
+        return `start_date=${start_date}&limit=100`
+      }
+      return `start_date=${start_date}&before_lt=${before_lt}&limit=100`
+    })()
+    const url = `${baseUrl}?${args}`
+    // res data is ordered by timestamp, lt desc
+    const res = await axios<AccountEvents>(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.TON_API_KEY}`,
+      },
+    })
+
+    return res
+  }
+
   for (;;) {
-    const baseUrl = `${process.env.TON_API_URL}/accounts/${routerAddress}/events`
     try {
-      const args =
-        `start_date=${timestamp}&limit=100` +
-        (beforeLt ? `&before_lt=${beforeLt}` : '')
-
-      const url = `${baseUrl}?${args}`
-      // res data is ordered by timestamp desc
-      const res = await axios<AccountEvents>(url, {
-        headers: {
-          Authorization: `Bearer ${process.env.TON_API_KEY}`,
-        },
-      })
-
+      const res = await fetch(timestamp, beforeLt)
       const accountEvents = sortBy(
         res.data.events.filter((e) => !e.in_progress),
         (e) => e.timestamp,
@@ -52,7 +60,44 @@ const fetchEvents = async ({
       }
 
       break
-    } catch (e) {
+    } catch (e: any) {
+      // TODO: remove, this code is awful. It is because of misbehavior of ton api.
+      // after ton api fix fetching issue remove below code (if clause).
+      if (
+        e.response.data.error ===
+        'parent tx for 72dbee85f0612047e3dc941015d2db5d not found'
+      ) {
+        const timestamp = 1714816555
+        const res1 = await fetch(timestamp, beforeLt)
+        const accountEvents1 = sortBy(
+          res1.data.events.filter((e) => !e.in_progress),
+          (e) => e.timestamp,
+          'asc',
+        )
+        events.push(...accountEvents1)
+        const startDate2 = 0
+        const beforeLt2 = 46288954000001
+        const res2 = await fetch(startDate2, beforeLt2)
+        const accountEvents2 = sortBy(
+          res2.data.events.filter((e) => !e.in_progress),
+          (e) => e.timestamp,
+          'asc',
+        )
+        events.push(...accountEvents2)
+
+        const nextFrom = res2.data.next_from
+        if (accountEvents2.length >= 100) {
+          info(`fetched with start_date, before_lt, ${timestamp} ${beforeLt}`)
+          // TON API limit is 100 events per request.
+          // If 100 events are found, we need to fetch evetns more precisely.
+          // It we don't this, we may miss some events.
+          info('More than 100 events found.')
+          // there should be padding. because 'before_lt' is exclusive with padding. (30 is empirical value)
+          beforeLt = nextFrom + LOGICAL_TIME_PADDING
+          info(`Try to fetch events beforeLt, ${beforeLt}`)
+        }
+        continue
+      }
       error(`Error fetching events in url, ${baseUrl}`)
       logError(e)
       // Sentry.captureException(e);
