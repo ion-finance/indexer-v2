a2af6baa55ab956c1a665564653702ce2640b776
devlbrian <brian@ionfi.xyz>
2024-05-20 17:16:59 +0900

    Change indexerState

---
+++ prisma/migrations/20240520081409_modify_indexerstate2/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastEventId` on the `IndexerState` table. All the data in the column will be lost.
+  - You are about to drop the column `value` on the `IndexerState` table. All the data in the column will be lost.
+  - Added the required column `toLt` to the `IndexerState` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "IndexerState" DROP COLUMN "lastEventId",
+DROP COLUMN "value",
+ADD COLUMN     "toLt" TEXT NOT NULL;
