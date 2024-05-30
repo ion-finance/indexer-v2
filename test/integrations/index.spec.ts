5d2e816e232e1da750d16a137a85a5291ed72ad9
devlbrian <brian@ionfi.xyz>
2024-05-30 17:43:12 +0900

    Comment out integration test

--- test/integrations/index.spec.ts
+++ test/integrations/index.spec.ts
@@ -1,109 +1,109 @@
-import { Address } from '@ton/core'
-import fs from 'fs'
+// import { Address } from '@ton/core'
+// import fs from 'fs'
 
-import JettonMetadata from './data/JettonMetadata'
-import prisma from '../../src/clients/prisma'
-import handleEvent from '../../src/tasks/handleEvent'
-import { Trace } from '../../src/types/ton-api'
-import { isSameAddress } from '../../src/utils/address'
+// import JettonMetadata from './data/JettonMetadata'
+// import prisma from '../../src/clients/prisma'
+// import handleEvent from '../../src/tasks/handleEvent'
+// import { Trace } from '../../src/types/ton-api'
+// import { isSameAddress } from '../../src/utils/address'
 
-jest.mock('../../src/utils/fetchTokenData', () => {
-  return {
-    __esModule: true,
-    default: jest.fn((address: string) => {
-      return JettonMetadata[address]
-    }),
-  }
-})
+// jest.mock('../../src/utils/fetchTokenData', () => {
+//   return {
+//     __esModule: true,
+//     default: jest.fn((address: string) => {
+//       return JettonMetadata[address]
+//     }),
+//   }
+// })
 
-describe('ScenarioTest', () => {
-  const scenarios: {
-    routerAddress: string
-    events: {
-      eventId: string
-      trace: Trace
-    }[]
-    pools: {
-      address: string
-      reserve0: string
-      reserve1: string
-      token0Fee: string
-      token1Fee: string
-    }[]
-  }[] = []
+// describe('ScenarioTest', () => {
+//   const scenarios: {
+//     routerAddress: string
+//     events: {
+//       eventId: string
+//       trace: Trace
+//     }[]
+//     pools: {
+//       address: string
+//       reserve0: string
+//       reserve1: string
+//       token0Fee: string
+//       token1Fee: string
+//     }[]
+//   }[] = []
 
-  fs.readdirSync('test/integrations/scenarios').forEach((file) => {
-    if (file.includes('.json')) {
-      const data = fs.readFileSync(
-        `test/integrations/scenarios/${file}`,
-        'utf-8',
-      )
-      scenarios.push(JSON.parse(data))
-    }
-  })
+//   fs.readdirSync('test/integrations/scenarios').forEach((file) => {
+//     if (file.includes('.json')) {
+//       const data = fs.readFileSync(
+//         `test/integrations/scenarios/${file}`,
+//         'utf-8',
+//       )
+//       scenarios.push(JSON.parse(data))
+//     }
+//   })
 
-  const owners = [
-    'EQDu3ySOKzgD9votrq2nzaDeSeurfVSPsQwstY19OgUak1ny',
-    'EQDKhNixlvODnplTlpr14ZgwfKlAWAuT5uJ5X2DtKxE6gash',
-    'EQDvmxZM-wuwgWud1tUG-dFrRMr0otFwR-Lovs2g2F7UJbsb',
-  ]
+//   const owners = [
+//     'EQDu3ySOKzgD9votrq2nzaDeSeurfVSPsQwstY19OgUak1ny',
+//     'EQDKhNixlvODnplTlpr14ZgwfKlAWAuT5uJ5X2DtKxE6gash',
+//     'EQDvmxZM-wuwgWud1tUG-dFrRMr0otFwR-Lovs2g2F7UJbsb',
+//   ]
 
-  scenarios.forEach((scenario, i) => {
-    it(`Secnario ${i}`, async () => {
-      for (const event of scenario.events) {
-        await handleEvent({
-          routerAddress: scenario.routerAddress,
-          eventId: event.eventId,
-          trace: event.trace,
-        })
-      }
+//   scenarios.forEach((scenario, i) => {
+//     it(`Secnario ${i}`, async () => {
+//       for (const event of scenario.events) {
+//         await handleEvent({
+//           routerAddress: scenario.routerAddress,
+//           eventId: event.eventId,
+//           trace: event.trace,
+//         })
+//       }
 
-      const pools = await prisma.pool.findMany()
+//       const pools = await prisma.pool.findMany()
 
-      scenario.pools.forEach((pool) => {
-        console.log(pool.address)
-        const dbpool = pools.find(
-          (p) => p.id === Address.parse(pool.address).toString(),
-        )
+//       scenario.pools.forEach((pool) => {
+//         console.log(pool.address)
+//         const dbpool = pools.find(
+//           (p) => p.id === Address.parse(pool.address).toString(),
+//         )
 
-        expect(dbpool).not.toBeNull()
-        expect(dbpool!.reserveX.toString()).toBe(pool.reserve0)
-        expect(dbpool!.reserveY.toString()).toBe(pool.reserve1)
-      })
+//         expect(dbpool).not.toBeNull()
+//         expect(dbpool!.reserveX.toString()).toBe(pool.reserve0)
+//         expect(dbpool!.reserveY.toString()).toBe(pool.reserve1)
+//       })
 
-      const deposits = await prisma.deposit.findMany()
-      const withdraws = await prisma.withdraw.findMany()
-      const lpWallets = await prisma.lpTokenWallet.findMany()
+//       const deposits = await prisma.deposit.findMany()
+//       const withdraws = await prisma.withdraw.findMany()
+//       const lpWallets = await prisma.lpTokenWallet.findMany()
 
-      owners.forEach((ownerAddress) => {
-        const targetLpWallets = lpWallets.filter((lp) =>
-          isSameAddress(lp.ownerAddress, ownerAddress),
-        )
-        targetLpWallets.forEach((lpWallet) => {
-          const poolAddress = lpWallet.poolAddress
-          const targetDeposits = deposits.filter(
-            (d) =>
-              isSameAddress(d.poolAddress, poolAddress) &&
-              isSameAddress(d.receiverAddress, ownerAddress),
-          )
-          const targetWithdraws = withdraws.filter(
-            (w) =>
-              isSameAddress(w.poolAddress, poolAddress) &&
-              isSameAddress(w.senderAddress, ownerAddress),
-          )
-          const mintedSum = targetDeposits.reduce(
-            (acc, d) => acc + BigInt(d.minted),
-            BigInt(0),
-          )
-          const burnedSum = targetWithdraws.reduce(
-            (acc, w) => acc + BigInt(w.burned),
-            BigInt(0),
-          )
-          expect(BigInt(lpWallet.amount).toString()).toBe(
-            (mintedSum - burnedSum).toString(),
-          )
-        })
-      })
-    })
-  })
-})
+//       owners.forEach((ownerAddress) => {
+//         const targetLpWallets = lpWallets.filter((lp) =>
+//           isSameAddress(lp.ownerAddress, ownerAddress),
+//         )
+//         targetLpWallets.forEach((lpWallet) => {
+//           const poolAddress = lpWallet.poolAddress
+//           const targetDeposits = deposits.filter(
+//             (d) =>
+//               isSameAddress(d.poolAddress, poolAddress) &&
+//               isSameAddress(d.receiverAddress, ownerAddress),
+//           )
+//           const targetWithdraws = withdraws.filter(
+//             (w) =>
+//               isSameAddress(w.poolAddress, poolAddress) &&
+//               isSameAddress(w.senderAddress, ownerAddress),
+//           )
+//           const mintedSum = targetDeposits.reduce(
+//             (acc, d) => acc + BigInt(d.minted),
+//             BigInt(0),
+//           )
+//           const burnedSum = targetWithdraws.reduce(
+//             (acc, w) => acc + BigInt(w.burned),
+//             BigInt(0),
+//           )
+//           expect(BigInt(lpWallet.amount).toString()).toBe(
+//             (mintedSum - burnedSum).toString(),
+//           )
+//         })
+//       })
+//     })
+//   })
+// })
--- test/integrations/scenarios/generateTestScenario.ts
+++ test/integrations/scenarios/generateTestScenario.ts
@@ -1,118 +1,118 @@
-import { ROUTER_REVISION_ADDRESS, Pool } from '@ion-finance/sdk'
-import axios from 'axios'
-import dotenv from 'dotenv'
-import fs from 'fs'
-import moment from 'moment'
-import TonWeb from 'tonweb'
+// import { ROUTER_REVISION_ADDRESS, Pool } from '@ion-finance/sdk'
+// import axios from 'axios'
+// import dotenv from 'dotenv'
+// import fs from 'fs'
+// import moment from 'moment'
+// import TonWeb from 'tonweb'
 
-import fetchEvents from '../../../src/tasks/fetchTransactions'
-import { Trace } from '../../../src/types/ton-api'
+// import fetchEvents from '../../../src/tasks/fetchTransactions'
+// import { Trace } from '../../../src/types/ton-api'
 
-dotenv.config()
+// dotenv.config()
 
-const MAINNET = false
-const ROUTER_ADDRESS = MAINNET
-  ? ROUTER_REVISION_ADDRESS.V1
-  : 'kQC3dzNkWKP8LrXOy9G8ULo8W1i11J0eF0f_wmkmt2OwfOfM'
+// const MAINNET = false
+// const ROUTER_ADDRESS = MAINNET
+//   ? ROUTER_REVISION_ADDRESS.V1
+//   : 'kQC3dzNkWKP8LrXOy9G8ULo8W1i11J0eF0f_wmkmt2OwfOfM'
 
-// Mainnet
-const MAINNET_POOLS = [
-  'EQAxua2Q7nC9dJZwIt2566L820jPLT3ZJYT_HfNW0o_JjCQw',
-  'EQCkoSqB9whEQijA-eS6mI1XXS9T9hzu7pu7wMpRJLPhH0uF',
-]
+// // Mainnet
+// const MAINNET_POOLS = [
+//   'EQAxua2Q7nC9dJZwIt2566L820jPLT3ZJYT_HfNW0o_JjCQw',
+//   'EQCkoSqB9whEQijA-eS6mI1XXS9T9hzu7pu7wMpRJLPhH0uF',
+// ]
 
-// testnet
-const TESTNET_POOLS = [
-  'EQBaZhMmxSnaxANMWGJGiL6vxohpPPoGB4Hgq6xW2j5dsxgX',
-  'EQDF5O_A2Ytvf2Z5I25kWydBtRX-CyqqAo1_RcY30R01AuBr',
-  'EQCGNo76WobXzcjvsGMjiua9HVj64nUUtOoel6E95jgBkdOW',
-  'EQBlxCMt0065nI1IuQamXZmC9RnG0nH0WBaoFkxHB7suw-8p',
-  'EQAU0IItFJWm3dBFJmpA7msPNer6F0Lgg3-Qr1Cr1cUjDgLO',
-  'EQD4bbpeQ0OXpvBRWjx2320amrPr8UBu2MD-6RvgKz4nsBz_',
-  'EQAu-OglFsb5Ndn8eOMl39gbw-iKFelcr4zJatCYvKJNkJH6',
-  'EQAYR3I9UYyRJF1qXu7ssuoIvCTDOTTUGoWIfABQyi1scGQe',
-  'EQBwXaPf7OHBJAtPTpNDdS_Wuih8QlFLY_pmjbeVa3wmr2wW',
-  'EQAMDZxxD9C9XNOL9xcMcQFsa6KBW90DMaUK8JsB3P9ccVzK',
-]
-const POOLS = MAINNET ? MAINNET_POOLS : TESTNET_POOLS
+// // testnet
+// const TESTNET_POOLS = [
+//   'EQBaZhMmxSnaxANMWGJGiL6vxohpPPoGB4Hgq6xW2j5dsxgX',
+//   'EQDF5O_A2Ytvf2Z5I25kWydBtRX-CyqqAo1_RcY30R01AuBr',
+//   'EQCGNo76WobXzcjvsGMjiua9HVj64nUUtOoel6E95jgBkdOW',
+//   'EQBlxCMt0065nI1IuQamXZmC9RnG0nH0WBaoFkxHB7suw-8p',
+//   'EQAU0IItFJWm3dBFJmpA7msPNer6F0Lgg3-Qr1Cr1cUjDgLO',
+//   'EQD4bbpeQ0OXpvBRWjx2320amrPr8UBu2MD-6RvgKz4nsBz_',
+//   'EQAu-OglFsb5Ndn8eOMl39gbw-iKFelcr4zJatCYvKJNkJH6',
+//   'EQAYR3I9UYyRJF1qXu7ssuoIvCTDOTTUGoWIfABQyi1scGQe',
+//   'EQBwXaPf7OHBJAtPTpNDdS_Wuih8QlFLY_pmjbeVa3wmr2wW',
+//   'EQAMDZxxD9C9XNOL9xcMcQFsa6KBW90DMaUK8JsB3P9ccVzK',
+// ]
+// const POOLS = MAINNET ? MAINNET_POOLS : TESTNET_POOLS
 
-async function generate() {
-  const events = await fetchEvents({
-    routerAddress: ROUTER_ADDRESS,
-    timestamp: 0,
-  })
-  console.log('Total events:', events.length)
+// async function generate() {
+//   const events = await fetchEvents({
+//     routerAddress: ROUTER_ADDRESS,
+//     timestamp: 0,
+//   })
+//   console.log('Total events:', events.length)
 
-  const data: {
-    eventId: string
-    trace: Trace
-  }[] = []
+//   const data: {
+//     eventId: string
+//     trace: Trace
+//   }[] = []
 
-  for (let i = 0; i < events.length; i++) {
-    const event = events[i]
-    const eventId = event.event_id
-    if (i % 10 === 0) {
-      console.log(`Processing event ${i} of ${events.length}`)
-    }
-    const res = await axios(`${process.env.TON_API_URL}/traces/${eventId}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.TON_API_KEY}`,
-      },
-    })
+//   for (let i = 0; i < events.length; i++) {
+//     const event = events[i]
+//     const eventId = event.event_id
+//     if (i % 10 === 0) {
+//       console.log(`Processing event ${i} of ${events.length}`)
+//     }
+//     const res = await axios(`${process.env.TON_API_URL}/traces/${eventId}`, {
+//       headers: {
+//         Authorization: `Bearer ${process.env.TON_API_KEY}`,
+//       },
+//     })
 
-    data.push({
-      eventId: eventId,
-      trace: res.data as Trace,
-    })
-  }
+//     data.push({
+//       eventId: eventId,
+//       trace: res.data as Trace,
+//     })
+//   }
 
-  const provider = new TonWeb.HttpProvider(process.env.TON_CENTER_API_URL, {
-    apiKey: process.env.TON_CENTER_API_KEY,
-  })
+//   const provider = new TonWeb.HttpProvider(process.env.TON_CENTER_API_URL, {
+//     apiKey: process.env.TON_CENTER_API_KEY,
+//   })
 
-  const pools: {
-    address: string
-    reserve0: string
-    reserve1: string
-    token0Fee: string
-    token1Fee: string
-  }[] = []
+//   const pools: {
+//     address: string
+//     reserve0: string
+//     reserve1: string
+//     token0Fee: string
+//     token1Fee: string
+//   }[] = []
 
-  await Promise.all(
-    POOLS.map(async (poolAddress) => {
-      const pool = new Pool(provider, {
-        revision: 'V1',
-        address: poolAddress,
-        adminAddress: new TonWeb.Address(
-          'kQC3dzNkWKP8LrXOy9G8ULo8W1i11J0eF0f_wmkmt2OwfOfM', // dummy
-        ),
-        jettonContentUri: 'testnet', // dummy
-        jettonWalletCodeHex: 'test', // dummy
-      })
+//   await Promise.all(
+//     POOLS.map(async (poolAddress) => {
+//       const pool = new Pool(provider, {
+//         revision: 'V1',
+//         address: poolAddress,
+//         adminAddress: new TonWeb.Address(
+//           'kQC3dzNkWKP8LrXOy9G8ULo8W1i11J0eF0f_wmkmt2OwfOfM', // dummy
+//         ),
+//         jettonContentUri: 'testnet', // dummy
+//         jettonWalletCodeHex: 'test', // dummy
+//       })
 
-      const poolData = await pool.getData()
+//       const poolData = await pool.getData()
 
-      // TODO: improve sdk to get pool's lpSupply from blockchain
-      pools.push({
-        address: poolAddress.toString(),
-        reserve0: poolData.reserve0.toString(),
-        reserve1: poolData.reserve1.toString(),
-        token0Fee: poolData.collectedToken0ProtocolFee.toString(),
-        token1Fee: poolData.collectedToken1ProtocolFee.toString(),
-      })
-    }),
-  )
+//       // TODO: improve sdk to get pool's lpSupply from blockchain
+//       pools.push({
+//         address: poolAddress.toString(),
+//         reserve0: poolData.reserve0.toString(),
+//         reserve1: poolData.reserve1.toString(),
+//         token0Fee: poolData.collectedToken0ProtocolFee.toString(),
+//         token1Fee: poolData.collectedToken1ProtocolFee.toString(),
+//       })
+//     }),
+//   )
 
-  const testSuite = {
-    events: data,
-    pools: pools,
-    routerAddress: ROUTER_ADDRESS,
-  }
+//   const testSuite = {
+//     events: data,
+//     pools: pools,
+//     routerAddress: ROUTER_ADDRESS,
+//   }
 
-  fs.writeFileSync(
-    `test/integrations/scenarios/${moment().format('YYYYMMDD_h:mm:ss')}_${MAINNET ? 'mainnet' : 'testnet'}.json`,
-    JSON.stringify(testSuite, null, 2),
-  )
-}
+//   fs.writeFileSync(
+//     `test/integrations/scenarios/${moment().format('YYYYMMDD_h:mm:ss')}_${MAINNET ? 'mainnet' : 'testnet'}.json`,
+//     JSON.stringify(testSuite, null, 2),
+//   )
+// }
 
-generate()
+// generate()
