5af350d5efb9719d7d95ba142016cb1d6f428373
devlbrian <brian@ionfi.xyz>
2024-05-30 17:29:10 +0900

    Rename 'upsertToken' -> 'createToken'

--- src/mappings/cpmm/createToken.ts
+++ src/mappings/cpmm/createToken.ts
@@ -2,7 +2,7 @@ import prisma from 'src/clients/prisma'
 import { changeNameOfProxyTon, changeSymbolOfProxyTon } from 'src/utils/address'
 import fetchTokenData from 'src/utils/fetchTokenData'
 
-export const upsertToken = async (tokenAddress: string, timestamp: string) => {
+export const createToken = async (tokenAddress: string, timestamp: string) => {
   const tokenData = await fetchTokenData(tokenAddress)
   if (tokenData) {
     const name = changeNameOfProxyTon(tokenAddress, tokenData.metadata.name)
@@ -10,18 +10,8 @@ export const upsertToken = async (tokenAddress: string, timestamp: string) => {
       tokenAddress,
       tokenData.metadata.symbol,
     )
-    return await prisma.token.upsert({
-      where: {
-        id: tokenAddress,
-      },
-      update: {
-        jettonMinterAddress: tokenData.minter_address,
-        name,
-        symbol,
-        decimals: parseInt(tokenData.metadata.decimals),
-        image: tokenData.metadata.image || '',
-      },
-      create: {
+    return await prisma.token.create({
+      data: {
         id: tokenAddress,
         jettonMinterAddress: tokenData.minter_address,
         name,
